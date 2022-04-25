51d7935cdd822761539afbbaa642e1b6242a0f48
Eric Chiu <ericwchiu@gmail.com>
2022-04-24 17:40:08 -0700

    updated Flask debug setting

--- api/app.py
+++ api/app.py
@@ -27,4 +27,4 @@ def hello_world():
     return "<h1>Please navigate GreenGoals using the mobile app.</h1>"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', debug=False)
