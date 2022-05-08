5bbbe31bdfe69e5ef89a47ebfd809374410f3c87
Eric Chiu <ericwchiu@gmail.com>
2022-05-07 21:05:20 -0700

    added API handling for assigning goals to a user and marking them as complete

--- api/user.py
+++ api/user.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify, make_response
-from models import UsersModel
+from models import UsersModel, UsersGoalsModel
 from app import db
 from fb_auth import verify_token
+from datetime import date
 
 bp = Blueprint('user', __name__, url_prefix='/users')
 
@@ -11,7 +12,7 @@ def get_post_users():
         users = UsersModel.query.all()
         results = [
             {
-                "user_id": user.goal_id,
+                "user_id": user.user_id,
                 "email": user.email,
                 "points": user.points
             }
@@ -78,4 +79,69 @@ def add_points(email):
         res.headers.set('Allow', ['POST'])
         res.status_code = 405
         return res
-        
+
+@bp.route('/<uid>/goals', methods=['GET', 'POST'])
+# @verify_token
+def goals_for_user(uid):
+    user = UsersModel.query.get_or_404(uid)
+
+    if request.method == 'POST':
+        if request.is_json:
+            content = request.get_json()
+
+            if 'goal_id' not in content:
+                error = {"Error": "The request object is missing at least one of the required attributes"}
+                return jsonify(error), 400
+
+            new_usergoal = UsersGoalsModel(user_id=uid, goal_id=content['goal_id'], date_assigned=date.today())
+            db.session.add(new_usergoal)
+            db.session.commit()
+
+            usergoal = {
+                "id": new_usergoal.id,
+                "user_id": new_usergoal.user_id,
+                "goal_id": new_usergoal.goal_id,
+                "date_assigned": new_usergoal.date_assigned,
+                "date_completed": new_usergoal.complete
+            }
+            return jsonify(usergoal), 201
+
+    elif request.method == 'GET':
+        if request.is_json:
+            content = request.get_json()
+
+            usersgoals = UsersGoalsModel.query.filter_by(user_id=user.user_id).all()
+            results = [
+                {
+                    "id": goal.id,
+                    "user_id": goal.user_id,
+                    "goal_id": goal.goal_id,
+                    "date_assigned": goal.date_assigned,
+                    "date_completed": goal.complete
+                } 
+            for goal in usersgoals]
+            return jsonify({"count": len(usersgoals), "goals": results}), 200 
+
+    else:
+        res = make_response({"Error": "Method not recognized"})
+        res.headers.set('Allow', ['GET', 'POST'])
+        res.status_code = 405
+        return res
+
+@bp.route('/<user_id>/goals/<id>', methods=['PATCH'])
+def update_goal_for_user(user_id, id):
+    user = UsersModel.query.get_or_404(user_id)
+    usersgoal = UsersGoalsModel.query.get_or_404(id)
+
+    if request.method == 'PATCH':
+        usersgoal.complete = True
+        db.session.add(usersgoal)
+        db.session.commit()
+
+        message = {"Message": "The goal was marked as completed for the user"}
+        return jsonify(message), 201
+    else:
+        res = make_response({"Error": "Method not recognized"})
+        res.headers.set('Allow', ['PATCH'])
+        res.status_code = 405
+        return res
