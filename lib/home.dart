326b9a86d8fec79d4e9f6eca1a7c7bb1f9466d1b
Justine Rivera <justinerivera@gmail.com>
2022-05-26 18:52:01 -0400

    Make homepage stateful

--- lib/home.dart
+++ lib/home.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:greengoals/goals_response.dart';
 import 'service.dart' as service;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Goal>>(
