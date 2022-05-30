7690f49a2e6cfdd750feb7bd87a5ca151ba4122b
Justine Rivera <justinerivera@gmail.com>
2022-05-29 21:30:13 -0400

    Center the progress indicator

--- lib/home.dart
+++ lib/home.dart
@@ -41,7 +41,17 @@ class _HomePageState extends State<HomePage> {
                 }
             );
           } else {
-            return const CircularProgressIndicator();
+            return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                  ],
+                )
+            );
+
+            //return const CircularProgressIndicator();
           }
         }
     );
